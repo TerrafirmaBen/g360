43ffa1234cf17d53690675c2b811c18a3bf10b46
TerrafirmaNina <58975064+TerrafirmaNina@users.noreply.github.com>
2020-09-29 11:14:44 +0100

    test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ import {helloWorldClass, helloWorldFunction} from './modules/example.js';
 import * as hw from  './modules/example.js';
 // import css
 import './styles/example.css'
+import "../node_modules/ol/ol.css";
 
-
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+var map = new Map({
+  view: new View({
+    center: [0, 0],
+    zoom: 1
+  }),
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  target: 'map'
+});
 
 hw.helloWorldFunction()
